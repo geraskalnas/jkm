5e6bb179cfb04b35c7d9e8ac54fec8c4a7515a00
Sajauskas Nojus <PG4339LA>
2019-03-23 11:04:06 +0200

    Update k3

--- source/repos/testai/k3/k3/a.cpp
+++ source/repos/testai/k3/k3/a.cpp
@@ -4,50 +4,65 @@
 
 using namespace std;
 
-int didzID, didzR, mas;
+int id, idArr, val;
+int spVal=0, spArrID, spID;
 
-void didziausias(int id, int val, int b=0) {
-	if (didzR < val) {
-		didzID = id;
-		didzR = val;
-		mas = b;
+ifstream fi("Duomenys.txt");
+
+void didziausias() {
+	int m1, m2, m3, m4, m5, m6;
+	fi >> id >> m1 >> m2 >> m3 >> m4 >> m5 >> m6;
+	int arr[] = { m1, m2, m3, m4, m5, m6 };
+	for (int i = 0; i < 6; i++) {
+		if (arr[i] > val) {
+			idArr = i;
+			val = arr[i];
+		}
 	}
 }
 
 int main() {
 	int t;
 
-	ifstream fi("Duomenys.txt");
-
 	fi >> t;
 	const int n = t;
 
-	int ids[6], vals[6];
+	int ids[50], vals[50];
 
-	int id, val;
 	for (int i = 0; i < n; i++) {
-		fi >> id >> val;
+		didziausias();
 		ids[i] = id;
 		vals[i] = val;
-		didziausias(id, val);
+		if (spVal < val) {
+			spVal = val;
+			spID = id;
+			spArrID = idArr;
+		}
+		id = 0;
+		val = 0;
 	}
-	fi.close();
 
-	cout << "Geriausias pasirodes sportininkas:" << endl << "  id: " << didzID << endl << "  reiksme: " << didzR << endl;
-
+	cout << "Geriausias pasirodes sportininkas:" << endl << "  id: " << spID << endl << "  reiksme: " << spVal << endl;
+	
 	ofstream fo("Rezultatai.txt");
 	cout << "Rusiavimas:" << endl;
 	for (int a = 0; a < n; a++) {
-		didzID = 0;
-		didzR = 0;
+		spVal = 0;
+		spID = 0;
+		spArrID = 0;
 		for (int b = 0; b < n; b++) {
-			didziausias(ids[b], vals[b], b);
+			if (spVal < vals[b]) {
+				spVal = vals[b];
+				spID = ids[b];
+				spArrID = b;
+			}
 		}
-		fo << left << "|" << setw(5) << didzID << didzR << "|" << endl;
-		cout << a + 1 << ". " << didzID << didzR << endl;
-		vals[mas] = 0;
+		fo << left << "|" << setw(5) << spID << spVal << "|" << endl;
+		cout << left << a + 1 << ". " << spID << " " << spVal << endl;
+		vals[spArrID] = 0;
 	}
 	fo.close();
+	
 
 	cin >> t;
 	return t;
